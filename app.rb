785d03915a7b97c3366143497aea02517d5f48cb
John Kisor <jkisor@gmail.com>
2013-11-08 01:19:33 -0800

    simple sign in and bootstrap

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require "sinatra"
 require "sinatra/activerecord"
+require 'sinatra/flash'
+
 Dir["./models/*.rb"].each { |file| require file }
+
+enable :sessions
 
 set :database, "sqlite3:///app.db"
 
@@ -22,5 +26,20 @@ post '/signup' do
 		redirect '/'
 	else
 		redirect 'signup'
+	end
+end
+
+get '/signin' do
+	erb :'signin'
+end
+
+post '/signin' do
+	@user = User.find_by(name: params[:user][:name])
+	if @user.nil?
+		flash.now[:danger] = "Invalid"
+		erb :'signin'
+	else
+		flash[:success] = "Welcome back"
+		redirect '/'
 	end
 end
---
+++ spec/integration/signin_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "signing in" do	
+	before { visit '/signin' }
+	subject { page }
+	it { should have_content('Sign in') }
+	context 'invalid credentials are entered' do
+		before { click_button "Sign in" }
+		it { should have_selector('div.alert.alert-danger', text: 'Invalid') }
+	end
+
+	context 'valid credentials are entered' do
+		let(:user) { User.create(name: 'testuser') }
+		before do
+			fill_in 'user_name', with: user.name
+			click_button "Sign in"
+		end
+		it { should have_selector('div.alert.alert-success', text: 'Welcome back') }
+	
+		# context "after signout" do
+		# 	before { click_button "Sign out" }
+		# 	it { should have_button('Sign in') }
+		# end
+	end
+
+
+
+end
